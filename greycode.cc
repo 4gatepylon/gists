565dea5a8421e3138bca93fdf64d3a16b34408b7
Adriano Hernandez <adrianoh@mit.edul>
2022-12-14 12:15:12 +0100

    ay lmao

---
+++ greycode.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // Every grey code can be made from a smaller grey code one way then back flipped
+    vector<int> grayCode(int n) {
+        if (n == 1) return vector<int>{0, 1};
+        else {
+            vector<int> gc = grayCode(n-1);
+            int N = gc.size();
+            for (int i = N - 1; i >= 0; i--) {
+                gc.push_back((gc[i] << 1) + 1);
+                gc[i] <<= 1;
+            }
+            return gc;
+        }
+    }
+};
